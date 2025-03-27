0935d107b6ca151c4f1a40042de0818ee518f15f
Aarya <aaryamaan.106@gmail.com>
2025-03-27 12:28:31 +0530

    first commint

---
+++ frontned/src/pages/AuthPage.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AuthPage=()=>{
+
+
+  return(
+    <div>hello
+      
+    </div>
+  )
+
+}
+  
+
+export default AuthPage;
